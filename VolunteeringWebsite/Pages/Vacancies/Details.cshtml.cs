261343015c5f52c59afa04244290ad2e5ad42c33
Serghei Codrean <serghei.codrean@hotmail.com>
2020-06-14 15:42:58 +0300

    Apply vacancy button

--- VolunteeringWebsite/Pages/Vacancies/Details.cshtml.cs
+++ VolunteeringWebsite/Pages/Vacancies/Details.cshtml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using VolunteeringWebsite.Areas.Identity.Data;
 using VolunteeringWebsite.Models;
 
 namespace VolunteeringWebsite.Vacancies
@@ -12,13 +14,16 @@ namespace VolunteeringWebsite.Vacancies
     public class DetailsModel : PageModel
     {
         private readonly VolunteeringWebsite.Models.VolunteeringDatabaseContext _context;
+        private readonly UserManager<VolunteeringWebsiteUser> _userManager;
 
-        public DetailsModel(VolunteeringWebsite.Models.VolunteeringDatabaseContext context)
+        public DetailsModel(VolunteeringWebsite.Models.VolunteeringDatabaseContext context, UserManager<VolunteeringWebsiteUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public Vacancy Vacancy { get; set; }
+        public bool IsApplied { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -37,7 +42,42 @@ namespace VolunteeringWebsite.Vacancies
             {
                 return NotFound();
             }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                var userVacancy = await _context.User_Vacancy.FirstOrDefaultAsync(uv => uv.VacancyId == id.Value && uv.UserId == user.Id);
+                IsApplied = userVacancy != null;
+            }
+
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostApplyAsync(int? id)
+        {
+            if (!id.HasValue)
+                return new JsonResult(new { applied = false });
+
+            var user = await _userManager.GetUserAsync(User);
+
+            var userVacancy = await _context.User_Vacancy.FirstOrDefaultAsync(uv => uv.VacancyId == id.Value && uv.UserId == user.Id);
+
+            if (userVacancy != null)
+            {
+                return new JsonResult(new { applied = false });
+            }
+            else
+            {
+                _context.User_Vacancy.Add(new User_Vacancy
+                {
+                    VacancyId = id.Value,
+                    UserId = user.Id
+                });
+                await _context.SaveChangesAsync();
+            }
+
+            return new JsonResult(new { applied = true });
         }
     }
 }
